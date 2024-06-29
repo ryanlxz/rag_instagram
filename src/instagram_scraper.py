177cbb0c4da3bd9cddb7d2d325faa233f203f15f
ryan <ryanlxz@gmail.com>
2024-06-29 17:37:46 +0800

    add function to update downloaded posts but it is not working properly yet

--- src/instagram_scraper.py
+++ src/instagram_scraper.py
@@ -20,7 +20,26 @@ class GetInstagramProfile:
     def download_users_profile_picture(self, username):
         self.L.download_profile(username, profile_pic_only=True)
 
-    def download_users_posts_with_periods(self, username: str):
+    def download_posts(
+        self, data_path: str, start_date: str, end_date: str, username: str
+    ):
+        """downloads posts from start_date to end_date into the data_path
+
+        Args:
+            data_path (str): folder path to save downloaded posts
+            start_date (str): start date of posts to download
+            end_date (str): end date of posts to download
+            username (str): instagram username
+        """
+        posts = instaloader.Profile.from_username(self.L.context, username).get_posts()
+        os.chdir(data_path)
+        for post in takewhile(
+            lambda p: p.date > start_date, dropwhile(lambda p: p.date > end_date, posts)
+        ):
+            self.L.download_post(post, username)
+        logger.info(f"downloaded {username} posts")
+
+    def download_user_posts(self, username: str):
         """Creates a new folder that is named as the Instagram Username, and downloads all the posts - text file, pictures in jpg, video in mp4.
         These files are named by the date posted.
 
@@ -29,15 +48,43 @@ class GetInstagramProfile:
         """
         data_path = Path("./data")
         data_path.mkdir(parents=True, exist_ok=True)
-        posts = instaloader.Profile.from_username(self.L.context, username).get_posts()
-        SINCE = datetime.strptime((conf["start_date"]), "%Y-%m-%d")
-        UNTIL = datetime.strptime((conf["end_date"]), "%Y-%m-%d")
+        end_date = datetime.today()
+        formatted_end_date = end_date.strftime("%Y-%m-%-d")
+        self.download_posts(
+            data_path=data_path,
+            start_date=conf["start_date"],
+            end_date=formatted_end_date,
+            username=username,
+        )
 
-        os.chdir(data_path)
-        for post in takewhile(
-            lambda p: p.date > SINCE, dropwhile(lambda p: p.date > UNTIL, posts)
-        ):
-            self.L.download_post(post, username)
+    def update_downloaded_posts(self, username: str):
+        """Update the downloaded posts by downloading the latest posts. First, check for the latest
+        file in the data folder, and then download the latest posts continuing from there.
+
+        Args:
+            username (str): username of instagram profile
+        """
+        folder = Path(f"./data/{username}")
+        if folder.exists():
+            files = [f for f in folder.iterdir() if f.is_file()]
+        else:
+            logger.error("No data folder found. Please download the posts first.")
+        file_datetime_list = []
+        for file in files:
+            datetime_str = file.name.split("_UTC")[0]
+            file_datetime = datetime.strptime(datetime_str, "%Y-%m-%d_%H-%M-%S")
+            file_datetime_list.append(file_datetime)
+        file_datetime_list.sort(reverse=True)
+        latest_file_datetime = file_datetime_list[0]
+        print(latest_file_datetime)
+        end_date = datetime.today()
+        # formatted_end_date = end_date.strftime("%Y-%m-%-d")
+        self.download_posts(
+            data_path=folder,
+            start_date=latest_file_datetime,
+            end_date=end_date,
+            username=username,
+        )
 
     def download_hastag_posts(self, hashtag):
         for post in instaloader.Hashtag.from_name(self.L.context, hashtag).get_posts():
@@ -67,7 +114,8 @@ class GetInstagramProfile:
 if __name__ == "__main__":
     cls = GetInstagramProfile()
     # cls.download_users_profile_picture("best_gadgets_2030")
-    cls.download_users_posts_with_periods(credentials["USERNAME"])
+    # cls.download_user_posts(credentials["USERNAME"])
+    cls.update_downloaded_posts(credentials["USERNAME"])
     # cls.download_hastag_posts("gadgets")
     # cls.get_users_followers("best_gadgets_2030")
     # cls.get_users_followings("best_gadgets_2030")
