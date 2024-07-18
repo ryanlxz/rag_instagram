6d491265db25631626aed8c65185bb355b1351ea
ryan <ryanlxz@gmail.com>
2024-07-18 20:53:10 +0800

    make directory to store log files

--- logs/logging.py
+++ logs/logging.py
@@ -1,6 +1,10 @@
 import logging
 import logging.config
 import yaml
+from pathlib import Path
+
+log_path = Path("./logs/log_files")
+log_path.mkdir(parents=True, exist_ok=True)
 
 with open("./logs/logging_conf.yml", "r") as f:
     config = yaml.safe_load(f.read())
